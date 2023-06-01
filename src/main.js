b923137ce34ba8721bd0f2a88613577985e240aa
rodarg877 <rodarg87@gmail.com>
2023-06-01 13:08:58 -0300

    implementacion de mapas para el detalle de la card

--- src/main.js
+++ src/main.js
@@ -4,13 +4,21 @@ import './bootstrap.js'
 import './form.js'
 import './axios.js'
 import './prime.js'
+import 'leaflet/dist/leaflet.css'
 
 import { router } from './router'
 import store from './store'
 
 import './globalMixins'
 import './globalFilters'
+import { Icon } from 'leaflet';
 
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 Vue.config.productionTip = false
 
 
