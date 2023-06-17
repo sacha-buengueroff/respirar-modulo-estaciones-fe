4b090f1a387808137c8acfb4b8ff867fd17e1baf
Adriel <adrielgentil@gmail.com>
2023-06-17 19:55:00 -0300

    Se modificó vue.config para fijar que el puerto de arranque siempre sea 8080, visto que se necesita especificar el mismo en el dockerfile

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080, // Cambia el número de puerto según tus preferencias
+  },
 })
