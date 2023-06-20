4ff326b614b958ef03dbafef15046a339a9ad067
Sacha <69225351+sacha-buengueroff@users.noreply.github.com>
2023-06-20 20:15:46 -0300

    Revert "Dockerfile"

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 8080, // De cambiarse este puerto debera actualizarse el dockerfile
-  },
+  transpileDependencies: true
 })
